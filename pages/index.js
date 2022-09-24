9d9a90493029e7e6912b1ee583f67db0a0797529
schadrack10 <schadrackbotombe@gmail.com>
2022-09-24 15:59:25 +0200

    added stuff

--- pages/index.js
+++ pages/index.js
@@ -12,10 +12,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js Bradon!</a>
-        </h1>
-
+    
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
